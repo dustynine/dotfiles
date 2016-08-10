bfd9043267b035ddadfbba463bb7ab26251e0c91
Dusty Nine <27400@protonmail.ch>
2016-08-11 01:04:27 +0700

    Add files via upload

--- pypanel.py
+++ pypanel.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
-import os, datetime, subprocess
+import os
+import datetime
+import subprocess
+from time import sleep
 
-def Date():
-    #_datetime=datetime.datetime.now.strftime("%A")
-    d=datetime.datetime.now()
-    _d=d.strftime("%d %B, %Y, %A ")
-    return _d
 
-def Time():
-    t=datetime.datetime.now()
-    _t=t.strftime("%T")
-    return _t
+def date():
+    d = datetime.datetime.now()
+    date = d.strftime("%d %B, %Y, %A ")
+    return date
 
-def Ram():
-    raw = subprocess.run(["free","-h"],stdout=subprocess.PIPE)
-    raw = str(raw).split()
-    ram = str(raw[15]).strip("\\nSwap:") + '/3.9G'
-    #full = raw[10]
-    #ram = raw[15],'/',raw[10]
-    #raw = raw.split()[1]
-    #value = str(raw).split()[6]
+def time():
+    t = datetime.datetime.now()
+    time = t.strftime("%T")
+    return time
+
+def ram():
+    raw = str(subprocess.run(["free","-h"],stdout=subprocess.PIPE)).split()
+    ram = 'RAM: ' + raw[15].strip("\\nSwap:") + '/3.9G'
     return ram
 
-#def Dspace():
+def disk():
+    raw = str(subprocess.run(["df","-h"],stdout=subprocess.PIPE)).split()
+    disk = '/home: ' + raw[41] + '/177G; /:' + raw[21] + '/69G;'
+    # 20 - all root, 21 - occup root, 40 - all home, 41 - occup home
+    # replace if needed
+    return disk
 
-#def Cpu():
+#def workspace():
 
-#def Wspace():
+
 
 while True:
     #print('%{A:firefox:} Firefox %{A}')
-    print("%{l}",Ram(),"%{c}",Time(),"%{r}",Date())
+    print("%{l}",disk(),ram(),"%{c}",time(),"%{r}",date())
+    #sleep(0.25)
